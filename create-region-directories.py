89cf77f82955b85d119e7207b6d137fd9e71ce81
patternizer <michael.a.taylor@uea.ac.uk>
2023-11-29 14:56:04 +0000

    Updated automation script to update image download in place effect

--- create-region-directories.py
+++ create-region-directories.py
@@ -13,12 +13,11 @@ import pandas as pd
 import os
 import shutil
 import fileinput
-from bs4 import BeautifulSoup
 import re
 
 # DEFINE: source directory containing files to copy to all region directories
 
-template_directory = "ar6-land/ESAF/"
+#template_directory = "ar6-land/ESAF/"
 
 # LOAD: IPCC AR6 land look-up table
 
@@ -31,6 +30,8 @@ for index, row in df.iterrows():
     id_value = row['id']
     name_value = row['name']
     abbrev_value = row['abbrev']
+
+    template_directory = "ar6-land/" + abbrev_value + "/"
 
     directory_path = os.path.join('.', abbrev_value)    
     if not os.path.exists(directory_path):
@@ -52,41 +53,37 @@ for index, row in df.iterrows():
         with open(destination_file_path, 'r') as file:
             file_content = file.read()
 
-        # REPLACE: title line and figure names in each HTML file relative to the region directory
+            # EXTRACT: timescale from file name
+
+        timescale = filename.split('-')[-1].split('.')[0]
+
+        # UPDATE: region HTML files with modifications
         
         modified_content = file_content.replace('<h1>E.Southern-Africa (ESAF)</h1>', f'<h1>{name_value} ({abbrev_value})</h1>')
         modified_content = modified_content.replace('-region-27', f'-region-{str(id_value).zfill(2)}')
-                
-        '''             
-        # CONVERT: ordered list to HTML table
         
-        soup = BeautifulSoup(modified_content, 'html.parser')
-        ol_tags = soup.find_all('ol')
-        for ol_tag in ol_tags:
+        modified_content = modified_content.replace('"../../figures/BA_Total-ipcc-ar6-land-region-timeseries-sum-yearly-region-27.png"', f'"../../figures/BA_Total-ipcc-ar6-land-region-timeseries-sum-{timescale}-region-{str(id_value).zfill(2)}.png"')
+        modified_content = modified_content.replace('"../../formatted_data/BA_Total-timeseries-yearly-region-27.csv"', f'"../../formatted_data/BA_Total-timeseries-{timescale}-region-{str(id_value).zfill(2)}.csv"')
+        modified_content = modified_content.replace('"../../formatted_data/BA_Total-stats-yearly-region-27.csv"', f'"../../formatted_data/BA_Total-stats-{timescale}-region-{str(id_value).zfill(2)}.csv"')                
 
-            # EXTRACT: list items from ordered list
+        modified_content = modified_content.replace('"../../figures/BA_Total_Forest_NonForest-ipcc-ar6-land-region-timeseries-sum-yearly-region-27.png"', f'"../../figures/BA_Total_Forest_NonForest-ipcc-ar6-land-region-timeseries-sum-{timescale}-region-{str(id_value).zfill(2)}.png"')
+        modified_content = modified_content.replace('"../../formatted_data/BA_Total_Forest_NonForest-timeseries-yearly-region-27.csv"', f'"../../formatted_data/BA_Total_Forest_NonForest-timeseries-{timescale}-region-{str(id_value).zfill(2)}.csv"')
+        modified_content = modified_content.replace('"../../formatted_data/BA_Total_Forest_NonForest-stats-yearly-region-27.csv"', f'"../../formatted_data/BA_Total_Forest_NonForest-stats-{timescale}-region-{str(id_value).zfill(2)}.csv"')                
 
-            list_items = ol_tag.find_all('li')
+        modified_content = modified_content.replace('"../../figures/Cem_Total-ipcc-ar6-land-region-timeseries-sum-yearly-region-27.png"', f'"../../figures/Cem_Total-ipcc-ar6-land-region-timeseries-sum-{timescale}-region-{str(id_value).zfill(2)}.png"')
+        modified_content = modified_content.replace('"../../formatted_data/Cem_Total-timeseries-yearly-region-27.csv"', f'"../../formatted_data/Cem_Total-timeseries-{timescale}-region-{str(id_value).zfill(2)}.csv"')
+        modified_content = modified_content.replace('"../../formatted_data/Cem_Total-stats-yearly-region-27.csv"', f'"../../formatted_data/Cem_Total-stats-{timescale}-region-{str(id_value).zfill(2)}.csv"')                
 
-            # CREATE: HTML table
+        modified_content = modified_content.replace('"../../figures/Cem_Total_Forest_NonForest-ipcc-ar6-land-region-timeseries-sum-yearly-region-27.png"', f'"../../figures/Cem_Total_Forest_NonForest-ipcc-ar6-land-region-timeseries-sum-{timescale}-region-{str(id_value).zfill(2)}.png"')
+        modified_content = modified_content.replace('"../../formatted_data/Cem_Total_Forest_NonForest-timeseries-yearly-region-27.csv"', f'"../../formatted_data/Cem_Total_Forest_NonForest-timeseries-{timescale}-region-{str(id_value).zfill(2)}.csv"')
+        modified_content = modified_content.replace('"../../formatted_data/Cem_Total_Forest_NonForest-stats-yearly-region-27.csv"', f'"../../formatted_data/Cem_Total_Forest_NonForest-stats-{timescale}-region-{str(id_value).zfill(2)}.csv"')                
 
-            table_tag = soup.new_tag('table')
-            for li in list_items:
-
-                tr_tag = soup.new_tag('tr')
-                td_tag = soup.new_tag('td')
-                td_tag.string = li.get_text()
-                tr_tag.append(td_tag)
-                table_tag.append(tr_tag)
-
-            # REPLACE: ordered list with HTML table
-
-            ol_tag.replace_with(table_tag)
-        '''            
+        modified_content = modified_content.replace('csv">csv</a>', 'csv" target="_self">csv</a>')
+        modified_content = modified_content.replace('target="_blank"', 'target="_self"')
+                                                                    
         # WRITE: modified content back to the file
                                         
         with open(destination_file_path, 'w') as file:
-            #file.write(str(soup))
             file.write(modified_content)
                                                                             
 #------------------------------------------------------------------------------
